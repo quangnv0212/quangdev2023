3580b14e3ea479b03942770a5083c29641d64675
Vu Quang <quangnv.0212@gmail.com>
2023-10-24 10:27:52 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import { config } from 'dotenv'
+import express from 'express'
+import { UPLOAD_VIDEO_DIR } from '~/constants/dir'
+import { defaultErrorHandler } from '~/middlewares/error.middlewares'
+import mediasRouter from '~/routes/medias.routes'
+import staticRouter from '~/routes/static.routes'
+import usersRouter from '~/routes/users.routes'
+import databaseService from '~/services/database.services'
+import { initFolder } from '~/utils/file'
+import cors from 'cors'
+import tweetsRouter from '~/routes/tweets.routes'
+import bookmarksRouter from '~/routes/bookmarks.routes'
+import likesRouter from '~/routes/likes.routes'
+import searchRouter from '~/routes/search.routes'
+import { createServer } from 'http'
+import conversationsRouter from '~/routes/conversations.routes'
+import initSocket from '~/utils/socket'
+import YAML from 'yaml'
+// import fs from 'fs'
+// import path from 'path'
+import swaggerUi from 'swagger-ui-express'
+import swaggerJsdoc from 'swagger-jsdoc'
+// const file = fs.readFileSync(path.resolve('twitter-swagger.yaml'), 'utf8')
+// const swaggerDocument = YAML.parse(file)
+
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'X clone (Twitter API)',
+      version: '1.0.0'
+    }
+  },
+  apis: ['./openapi/*.yaml'] // files containing annotations as above
+}
+const openapiSpecification = swaggerJsdoc(options)
+
+config()
+databaseService.connect().then(() => {
+  databaseService.indexUsers()
+  databaseService.indexRefreshTokens()
+  // databaseService.indexVideoStatus()
+  databaseService.indexFollowers()
+  databaseService.indexTweets()
+})
+const app = express()
+const httpServer = createServer(app)
+app.use(cors())
+const port = process.env.PORT || 4000
+
+// Tạo folder upload
+initFolder()
+app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification))
+app.use('/users', usersRouter)
+app.use('/medias', mediasRouter)
+app.use('/tweets', tweetsRouter)
+app.use('/bookmarks', bookmarksRouter)
+app.use('/likes', likesRouter)
+app.use('/search', searchRouter)
+app.use('/conversations', conversationsRouter)
+app.use('/static', staticRouter)
+app.use('/static/video', express.static(UPLOAD_VIDEO_DIR))
+app.use(defaultErrorHandler)
+
+initSocket(httpServer)
+
+httpServer.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ src/services/database.services.ts
@@ -0,0 +1,110 @@
+import { MongoClient, Db, Collection } from 'mongodb'
+import { config } from 'dotenv'
+import User from '~/models/schemas/User.schema'
+import RefreshToken from '~/models/schemas/RefreshToken.schema'
+import Follower from '~/models/schemas/Follower.schema'
+import VideoStatus from '~/models/schemas/VideoStatus.schema'
+import Tweet from '~/models/schemas/Tweet.schema'
+import Hashtag from '~/models/schemas/Hashtag.schema'
+import Bookmark from '~/models/schemas/Bookmark.schema'
+import Like from '~/models/schemas/Like.schema'
+import Conversation from '~/models/schemas/Conversations.schema'
+config()
+
+const uri = `mongodb+srv://teoquoc0214:zJXixvqg4LsCniiN@cluster0.hy87mhy.mongodb.net/?retryWrites=true&w=majority`
+
+class DatabaseService {
+  private client: MongoClient
+  private db: Db
+  constructor() {
+    this.client = new MongoClient(uri)
+    this.db = this.client.db(process.env.DB_NAME)
+  }
+
+  async connect() {
+    try {
+      // Send a ping to confirm a successful connection
+      await this.db.command({ ping: 1 })
+      console.log('Pinged your deployment. You successfully connected to MongoDB!')
+    } catch (error) {
+      console.log('Error', error)
+      throw error
+    }
+  }
+
+  async indexUsers() {
+    const exists = await this.users.indexExists(['email_1_password_1', 'email_1', 'username_1'])
+
+    if (!exists) {
+      this.users.createIndex({ email: 1, password: 1 })
+      this.users.createIndex({ email: 1 }, { unique: true })
+      this.users.createIndex({ username: 1 }, { unique: true })
+    }
+  }
+  async indexRefreshTokens() {
+    const exists = await this.refreshTokens.indexExists(['exp_1', 'token_1'])
+
+    if (!exists) {
+      this.refreshTokens.createIndex({ token: 1 })
+      this.refreshTokens.createIndex(
+        { exp: 1 },
+        {
+          expireAfterSeconds: 0
+        }
+      )
+    }
+  }
+  async indexVideoStatus() {
+    const exists = await this.videoStatus.indexExists(['name_1'])
+
+    if (!exists) {
+      this.videoStatus.createIndex({ name: 1 })
+    }
+  }
+  async indexFollowers() {
+    const exists = await this.followers.indexExists(['user_id_1_followed_user_id_1'])
+    if (!exists) {
+      this.followers.createIndex({ user_id: 1, followed_user_id: 1 })
+    }
+  }
+  async indexTweets() {
+    const exists = await this.tweets.indexExists(['content_text'])
+    if (!exists) {
+      this.tweets.createIndex({ content: 'text' }, { default_language: 'none' })
+    }
+  }
+  get tweets(): Collection<Tweet> {
+    return this.db.collection(process.env.DB_TWEETS_COLLECTION as string)
+  }
+
+  get users(): Collection<User> {
+    return this.db.collection(process.env.DB_USERS_COLLECTION as string)
+  }
+
+  get refreshTokens(): Collection<RefreshToken> {
+    return this.db.collection(process.env.DB_REFRESH_TOKENS_COLLECTION as string)
+  }
+
+  get followers(): Collection<Follower> {
+    return this.db.collection(process.env.DB_FOLLOWERS_COLLECTION as string)
+  }
+  get videoStatus(): Collection<VideoStatus> {
+    return this.db.collection(process.env.DB_VIDEO_STATUS_COLLECTION as string)
+  }
+  get hashtags(): Collection<Hashtag> {
+    return this.db.collection(process.env.DB_HASHTAGS_COLLECTION as string)
+  }
+  get bookmarks(): Collection<Bookmark> {
+    return this.db.collection(process.env.DB_BOOKMARKS_COLLECTION as string)
+  }
+  get likes(): Collection<Like> {
+    return this.db.collection(process.env.DB_LIKES_COLLECTION as string)
+  }
+  get conversations(): Collection<Conversation> {
+    return this.db.collection(process.env.DB_VIDEO_STATUS_CONVERSATION as string)
+  }
+}
+
+// Tạo object từ class DatabaseService
+const databaseService = new DatabaseService()
+export default databaseService
